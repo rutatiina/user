604338f6ac3ce7c3fb886af90bede7bd73bcdbfd
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-09-28 13:13:17 +0300

    Update routing

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -36,3 +36,48 @@ Route::group(['middleware' => ['web', 'auth', 'tenant']], function() {
     ]);
 
 });
+
+/*
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+*/
+/*
++--------+----------+------------------------+------------------+------------------------------------------------------------------------+------------+
+| Domain | Method   | URI                    | Name             | Action                                                                 | Middleware |
++--------+----------+------------------------+------------------+------------------------------------------------------------------------+------------+
+|        | GET|HEAD | login                  | login            | App\Http\Controllers\Auth\LoginController@showLoginForm                | web        |
+|        |          |                        |                  |                                                                        | guest      |
+|        | POST     | login                  |                  | App\Http\Controllers\Auth\LoginController@login                        | web        |
+|        |          |                        |                  |                                                                        | guest      |
+|        | POST     | logout                 | logout           | App\Http\Controllers\Auth\LoginController@logout                       | web        |
+|        | GET|HEAD | password/confirm       | password.confirm | App\Http\Controllers\Auth\ConfirmPasswordController@showConfirmForm    | web        |
+|        |          |                        |                  |                                                                        | auth       |
+|        | POST     | password/confirm       |                  | App\Http\Controllers\Auth\ConfirmPasswordController@confirm            | web        |
+|        |          |                        |                  |                                                                        | auth       |
+|        | POST     | password/email         | password.email   | App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail  | web        |
+|        | GET|HEAD | password/reset         | password.request | App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm | web        |
+|        | POST     | password/reset         | password.update  | App\Http\Controllers\Auth\ResetPasswordController@reset                | web        |
+|        | GET|HEAD | password/reset/{token} | password.reset   | App\Http\Controllers\Auth\ResetPasswordController@showResetForm        | web        |
+|        | GET|HEAD | register               | register         | App\Http\Controllers\Auth\RegisterController@showRegistrationForm      | web        |
+|        |          |                        |                  |                                                                        | guest      |
+|        | POST     | register               |                  | App\Http\Controllers\Auth\RegisterController@register                  | web        |
+|        |          |                        |                  |                                                                        | guest      |
++--------+----------+------------------------+------------------+------------------------------------------------------------------------+------------+
+*/
+
