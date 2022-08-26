cc2835b587f3cccb17b91fb4f5fb8d4ef19bcf48
rutatiina <rutatiina@ruginem.org>
2022-08-26 20:31:07 +0300

    Updates

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -56,10 +56,10 @@ Route::group(['middleware' => ['web']], function() {
     Route::post('register', [RegisterController::class, 'register']);
 
     // Password Reset Routes...
-    Route::get('password/reset', ['Auth\ForgotPasswordController@showLinkRequestForm'])->name('password.request');
-    Route::post('password/email', ['Auth\ForgotPasswordController@sendResetLinkEmail'])->name('password.email');
-    Route::get('password/reset/{token}', ['Auth\ResetPasswordController@showResetForm'])->name('password.reset');
-    Route::post('password/reset', ['Auth\ResetPasswordController@reset']);
+    Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password/reset', [ResetPasswordController::class, 'reset']);
 
     Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
     Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
